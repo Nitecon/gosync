fa5675b1b4ae9e503552ba2a2766ea732dd40bd1
Will Hattingh <will.hattingh@neustar.biz>
2015-03-16 16:35:54 -0400

    Install the deps without having to list

--- install_deps.sh
+++ install_deps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in `go list -f '{{.Deps}}' | tr "[" " " | tr "]" " " | xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}' |grep  "\."`; do echo $i; done
+for i in `go list -f '{{.Deps}}' | tr "[" " " | tr "]" " " | xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}' |grep  "\."`; do go get -u $i; done
