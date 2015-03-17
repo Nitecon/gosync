294c59d2a4e1f6689970f895badf359dc4b86a90
Will Hattingh <will.hattingh@neustar.biz>
2015-03-17 16:15:03 -0400

    Using godeps for depsolving

--- jenkins_build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-for i in `go list -f '{{.Deps}}' | tr "[" " " | tr "]" " " | xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}' |grep  "\."`; do go get $i; done
-go build main.go
